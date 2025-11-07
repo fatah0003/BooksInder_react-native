062331d3cf428095cd70f1967fb3704e5edcab46
fatah0003 <fatahutd@gmail.com>
2025-11-07 13:21:12 +0100

    Ajout entity User, fixtures, config JWT et sécurité

---
+++ back/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('fatah@live.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->passwordHasher->hashPassword($user, '123'));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
